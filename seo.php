adafff5e03fe8ff450f7dcdb7122512aa76a42d3
Azeem <azeemrsd@gmail.com>
2022-02-02 01:06:50 +0400

    services pages added

---
+++ seo.php
@@ -0,0 +1,102 @@
+<?php include('includes/top-html.php') ?>
+	<title>Welcome to Eveblocs | Portfolio</title>
+	<?php include("includes/css.php") ?>
+</head>
+<body>
+<!-- Header Begin -->
+	<?php include('includes/header.php') ?>
+<!-- Header End -->
+
+<!-- Hero Banner Begins -->
+  <section class="heroBannerSection noBgImage">
+    <?php include('includes/lineContainer.php') ?>
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-lg-6 p-0 pe-4">
+          <div class="heroBannerImg pe-5">
+            <img src="images/seo-top-banner-illustration.svg" alt="">
+          </div>
+        </div>
+        <div class="col-12 col-lg-6 p-0">
+          <h2 class="mb-4">
+            <span class="primary-color">Search Engine</span>
+            <span class="text-light">Optimization</span> 
+          </h2>
+          <p class="topBiggerSubText fst-italic">
+            Grow your organic traffic, boost your search engine rankings and skyrocket your leads with our SEO services
+          </p>
+          <button class="ctaButtonContainer ctaButton mt-4" buttonval="Get a Qoute">Get a Qoute</button>
+        </div>
+      </div>
+    </div>
+  </section>
+<!-- Hero Banner Ends -->
+
+<section class="subHeroSection py-5">
+  <?php include('includes/lineContainer.php') ?>
+  <div class="container p-0">
+    <h2 class="heighlightedText text-center">
+      <span class="recoleta-light">Happy Search Engines =</span> Higher Rankings
+    </h2>
+    <div class="d-flex">
+      <div class="offset-lg-3 col-12 col-lg-9">
+        <p>
+          ﻿Search engine optimization is all about making sure that a website and by extension the brand, gains traction and visibility. It is the continuous process of improving the ranking of a website on search engines, which is crucial for ensuring that consumers find your business when they are on the lookout for your brand, products or services on search engines such as Google, Yahoo! and Bing.
+          <br>
+          SEO deals comprehensively with keyword research and analysis, recommendations, on-page and off-page changes such as creation of backlinks, which help in generating more traffic.
+        </p>
+      </div>
+    </div>
+    <h5 class="roboto-medium my-5">Research has shown that:</h5>
+  </div>
+  <div class="card__main__container percentageCard_container horizontal__scroll__container mt-3">
+    <div class="card__container">
+      <div class="card__body">
+        <h3 class="primaryColor">48%</h3>
+        <p> of people stated that a website’s design is the No. 1 factor in deeming the credibility of a business. </p>
+      </div>
+    </div>
+    <div class="card__container">
+      <div class="card__body">
+        <h3 class="primaryColor">95%</h3>
+        <p>of users indicated that a positive user experience is the most important factor when they visit a website.</p>
+      </div>
+    </div>
+    <div class="card__container">
+      <div class="card__body">
+        <h3 class="primaryColor">74%</h3>
+        <p>of users may return to a website again if it has been properly optimized for mobile usage.</p>
+      </div>
+    </div>
+    <div class="card__container">
+      <div class="card__body">
+        <h3 class="primaryColor">73%</h3>
+        <p>of companies are now investing in website design to differentiate their brands.</p>
+      </div>
+    </div>
+    <div class="card__container">
+      <div class="card__body">
+        <h3 class="primaryColor">62%</h3>
+        <p>of companies increased their sales by designing responsive mobile platforms for their websites.</p>
+      </div>
+    </div>
+  </div>
+  <div class="container p-0 my-5">
+    <div class="d-flex flex-wrap">
+      <div class="col-12 col-lg-6 pe-5">
+        <p>
+          Our web development experts ensure that aspects like site navigation, colour scheme graphics, layouts are in tandem with your branding. Additionally, we optimise your website for search engines, to improve your online rankings and attract your target audience.
+        </p>
+      </div>
+      <div class="col-12 col-lg-6">
+        <p>
+          At EvoBlocs, we do not just design websites. Regardless of the scale of your business, we transform them into communication channels that drive online growth and deliver the results that you need. We can help you build positive, lasting effects that encourage your consumers to return and also recommend your business to others.
+        </p>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!-- footer begin -->
+<?php include('includes/footer.php') ?>
+<!-- footer End -->
---
+++ services.php
@@ -0,0 +1,151 @@
+<?php include('includes/top-html.php') ?>
+	<title>Welcome to Eveblocs | Portfolio</title>
+	<?php include("includes/css.php") ?>
+</head>
+<body>
+<!-- Header Begin -->
+	<?php include('includes/header.php') ?>
+<!-- Header End -->
+
+<!-- Hero Banner Begins -->
+  <section class="heroBannerSection noBgImage">
+    <?php include('includes/lineContainer.php') ?>
+    <div class="container">
+      <div class="row">
+        <div class="offset-sm-3 col-md-9 p-0">
+              <h2 class="mb-4">
+                <span class="text-light">Our</span> 
+                <span class="primary-color">Services</span>
+              </h2>
+              <p>
+                Lorem, ipsum, dolor sit amet consectetur adipisicing elit. Ipsam illo asperiores rerum labore neque libero fugit cupiditate quod, minima, iure ratione doloribus. Id sint ut fuga architecto, tenetur earum, nam?   
+              </p>
+        </div>
+      </div>
+    </div>
+  </section>
+<!-- Hero Banner Ends -->
+
+<section class="services_box_section pb-5">
+  <?php include('includes/lineContainer.php') ?>
+  <div class="container p-0">
+    <div class="row gy-5 mb-5">
+      <div class="col-12">
+        <div class="seo_box_container seo-box d-flex">
+            <div class="__img">
+              <img src="images/seo-illustration.svg" alt="">
+            </div>
+            <div class="__content">
+              <h3>Search Engine Optimization (SEO)</h3>
+              <p class="note_text">Be Seen, Be Heard, Expand Your Growth</p>
+              <p>
+                We make sure that your brand soars upwards in organic rankings of any search engine and makes it to that all-important first results page. Focusing on proper website structure, clear information architecture, informative metadata, markups and combining the elements with outstanding content, we provide users with the experience and information they want. ...
+              </p>
+              <div class="cta__buttons">
+                <button class="ctaButtonContainer whiteButton ctaButton" buttonval="Know More">Know More</button>
+                <button class="ctaButtonContainer blackButton ctaButton" buttonval="Let's discuss your project">Let's discuss your project</button>
+              </div>
+            </div>
+        </div>
+      </div>
+    </div>
+    <div class="d-flex gy-5 mb-5 p-0">
+      <div class="col-12 col-lg-6 ps-0 pe-4">
+        <div class="seo_box_container smo-box d-flex flex-column">
+            <div class="__img">
+              <img src="images/social-media-illustration.png" alt="">
+            </div>
+            <div class="__content">
+              <h3>Social Media Optimization (SMO)</h3>
+              <p class="note_text">Build Authentic Connections with your audience</p>
+              <p>
+                Share your story and grow your brand. With a social media strategy that defines a target audience and solidifies a brand mission and voice, we reach out with paid and organic posts, allowing users to easily join your community, visit your website or make a purchase. Increasing engagement across the entire consumer acquisition flow, we will ...
+              </p>
+              <div class="cta__buttons">
+                <button class="ctaButtonContainer secondaryButton blackNWhite" buttonval="Know More">Know More</button>
+                <button class="ctaButtonContainer blackButton __withBlack ctaButton" buttonval="Let's discuss your project">Let's discuss your project</button>
+              </div>
+            </div>
+        </div>
+      </div>      
+      <div class="col-12 col-lg-6 ps-4 pe-0">
+        <div class="seo_box_container paid-promo-box d-flex flex-column">
+            <div class="__content">
+              <h3>Paid Advertising</h3>
+              <p class="note_text">Improve Your ROI with Scalable PPC Marketing </p>
+              <p>
+                Drive serious engagement for your business with well-placed advertising campaigns. Helping you run effective ads - our experts target the right keywords for your audiences at the right points in their purchasing journey. Driving traffic to your storefront from multiple carefully selected sources, we can reach your potential customers with buying intent and continually drive more quality leads through campaign optimization...v
+              </p>
+              <div class="cta__buttons">
+                <button class="ctaButtonContainer secondaryButton blackNWhite ctaButton" buttonval="Know More">Know More</button>
+                <button class="ctaButtonContainer blackButton __withBlack ctaButton" buttonval="Let's discuss your project">Let's discuss your project</button>
+              </div>
+            </div>
+            <div class="__img">
+              <img src="images/paid-ads-illustration.svg" alt="">
+            </div>
+        </div>
+      </div>
+    </div>
+    <div class="row gy-5">
+      <div class="col-12">
+        <div class="seo_box_container web-dev-box d-flex">
+            <div class="__content">
+              <h3 class="primaryColor">Web Design & Development</h3>
+              <p class="note_text">Result-Driven Solutions for Intuitive and Immersive Web Experiences</p>
+              <p>
+                Let your business shine across all digital assets. We design responsive websites and applications that are guided by research, focused on the end-user and engineered to meet business goals. Our team of experienced developers build scalable front-end and back-end solutions that are innovative and functional. We’ll help you tell a story with a stunning digital asset ….
+              </p>
+              <div class="cta__buttons">
+                <button class="ctaButtonContainer whiteButton ctaButton" buttonval="Know More">Know More</button>
+                <button class="ctaButtonContainer ctaButton" buttonval="Let's discuss your project">Let's discuss your project</button>
+              </div>
+            </div>
+            <div class="__img">
+              <img src="images/web-dev-illustration.svg" alt="">
+            </div>
+        </div>
+      </div>
+      <div class="col-12">
+        <div class="seo_box_container mob-app-box d-flex">
+            <div class="__img">
+              <img src="images/mobile-app-illustration.svg" alt="">
+            </div>
+            <div class="__content">
+              <h3>Mobile App Development</h3>
+              <p class="note_text">Perfect Blend of Form and Function</p>
+              <p>
+                EvoBlocs uses the latest technologies and frameworks to give you mobile applications that are not only built with high-quality code but are stunning, intuitive and the perfect representation of your brand. Our agile process ensures the best results, as we design apps keeping in mind the habits and desires of the end-user. Our customised mobile applications can help ...
+              </p>
+              <div class="cta__buttons">
+                <button class="ctaButtonContainer whiteButton ctaButton" buttonval="Know More">Know More</button>
+                <button class="ctaButtonContainer blackButton ctaButton" buttonval="Let's discuss your project">Let's discuss your project</button>
+              </div>
+            </div>
+        </div>
+      </div>
+      <div class="col-12">
+        <div class="seo_box_container content-writing-box d-flex">
+          <div class="__content">
+            <h3 class="primaryColor">Content Writing </h3>
+            <p class="note_text">Inform, Entertain, Engage and Convert</p>
+            <p>
+              Rising above the noise, our content strategists and writers inspire new and loyal consumers with high-quality, highly discoverable content. Thinking beyond discoverability, we make your brand relatable and grow its reach and personality with meaningful content that resonates with your target audience, every time. We help clients develop content strategies, steer their ...
+            </p>
+            <div class="cta__buttons">
+              <button class="ctaButtonContainer whiteButton ctaButton" buttonval="Know More">Know More</button>
+              <button class="ctaButtonContainer primaryButton ctaButton" buttonval="Let's discuss your project">Let's discuss your project</button>
+            </div>
+          </div>
+          <div class="__img">
+            <img src="images/content-writing.svg" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!-- footer begin -->
+<?php include('includes/footer.php') ?>
+<!-- footer End -->
